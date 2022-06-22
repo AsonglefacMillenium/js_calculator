26ec7835b53bcebdcf54ba35192d1294109491ab
Asonglefac Millenium <asonglefacmillenium@gmail.com>
2022-06-23 00:06:55 +0100

    simple calculator

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function calculator(a, b) {
+    let sum = a + b
+    let product = a * b
+    let sub = a - b
+    let div = a / b
+
+
+    document.write('<p> The sum is ' +sum+ '</p>')
+    document.write('<p> The product is ' +product+ '</p>')
+    document.write('<p> The difference is ' +sub+ '</p>')
+    document.write('<p> The division is ' +div+ '</p>')
+}
+
+
+calculator(10, 5);
